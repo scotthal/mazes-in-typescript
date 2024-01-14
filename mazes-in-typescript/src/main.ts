87f445acfd8a1cb12a0c76733f1be972fe03a519
Scott Hallock <scott@hallocks.net>
2024-01-14 20:50:31 +0000

    Initial rendering setup

--- mazes-in-typescript/src/main.ts
+++ mazes-in-typescript/src/main.ts
@@ -1,1 +1,22 @@
 import "./style.css";
+
+const app = document.querySelector("#app")! as HTMLDivElement;
+const canvas = document.createElement("canvas") as HTMLCanvasElement;
+app.appendChild(canvas);
+
+const ctx = canvas.getContext("2d")!;
+
+function animate() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 1;
+  ctx.moveTo(0, 0);
+  ctx.lineTo(canvas.width, canvas.height);
+  ctx.stroke();
+  window.requestAnimationFrame(animate);
+}
+
+animate();
